6cee4996e2c1e044369115aac9c4883364441e28
Navodya <127787000+manamendraJN@users.noreply.github.com>
2026-01-24 11:22:40 +0530

    Add components module with Message functional component and update App.jsx

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,10 +1,12 @@
 import Hello from './components/Hello'
+import Message from './components/Message'
 
 function App() {
   return (
     <div>
       <h1>React Fundamentals</h1>
       <Hello />
+      <Message />
     </div>
   )
 }
