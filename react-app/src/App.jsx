011039ff56bf42ae417e3b354ed8c0d8fb13228a
Navodya <127787000+manamendraJN@users.noreply.github.com>
2026-01-25 00:12:12 +0530

    remove the hello component

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,4 +1,3 @@
-import Hello from './components/Hello'
 import Message from './components/Message'
 import User from './components/User'
 import Counter from './components/Counter'
@@ -8,13 +7,13 @@ function App() {
     <div>
       <h1>React Fundamentals</h1>
 
-      <Hello />
       <Message />
 
       <User name="Navodya" age={22} />
       <User name="Alex" age={25} />
 
       <Counter />
+      
     </div>
   )
 }
